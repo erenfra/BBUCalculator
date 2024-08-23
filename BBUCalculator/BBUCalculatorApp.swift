830e1fa17d2f74d65910163c38f5cf26f470de65
Renato Oliveira Fraga <rofraga74@gmail.com>
2024-08-23 17:03:01 -0500

    Initial Commit

---
+++ BBUCalculator/BBUCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBUCalculatorApp.swift
+//  BBUCalculator
+//
+//  Created by Renato Oliveira Fraga on 8/23/24.
+//
+
+import SwiftUI
+
+@main
+struct BBUCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
